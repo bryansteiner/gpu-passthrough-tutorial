b6a09c518fa453c646f06a6d410cc6cc8274508b
Tristan <WindfallProphet@users.noreply.github.com>
2020-03-24 17:33:28 -0400

    schedutil change for Arch

--- kvm/hooks/release/end/cpu_mode_ondemand.sh
+++ kvm/hooks/release/end/cpu_mode_ondemand.sh
@@ -5,5 +5,5 @@ source "/etc/libvirt/hooks/kvm.conf"
 
 ## Enable CPU governor on-demand mode
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "ondemand" > $file; done
+for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "schedutil" > $file; done
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
